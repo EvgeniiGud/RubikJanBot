4d9db5de36910598c0f938aa45bffbe4bc34a81a
EgeniiGud <info_gev@bk.ru>
2025-07-04 16:24:49 +0400

    Изменил наименование.

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ bot = AsyncTeleBot(BOT_TOKEN)
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
 async def send_welcome(message):
-    text = 'Привет, Лиана.\nJust write me something and I will repeat it!'
+    text = 'Привет, Камиль.\nJust write me something and I will repeat it!'
     await bot.reply_to(message, text)
 
 
